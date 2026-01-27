5016f7066ad896d1ec5eb9af6736d6e1105aaaf4
Mister Lokans <mrlokans@gmail.com>
2026-01-27 20:43:06 +0100

    Prettify stockfish path resolution

--- blunder_tutor/web/config.py
+++ blunder_tutor/web/config.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import argparse
+import typing
 from pathlib import Path
 
 from pydantic import BaseModel
@@ -26,24 +27,36 @@ class AppConfig(BaseModel):
     data: DataConfig = DataConfig()
 
 
-def config_factory(parsed_args: argparse.Namespace, environ: dict) -> AppConfig:
-    default_stockfish = None
-    for path in [
+def get_engine_path(environ: typing.Mapping) -> str:
+    engine_path = environ.get("STOCKFISH_BINARY")
+    if engine_path is not None:
+        return engine_path
+    for path in (
         "/usr/games/stockfish",
         "/usr/local/bin/stockfish",
         "/usr/bin/stockfish",
-    ]:
+    ):
         if Path(path).exists():
-            default_stockfish = path
-            break
-    engine_path = environ.get("STOCKFISH_BINARY", default_stockfish)
-    final_engine_path = parsed_args and parsed_args.engine_path or engine_path
+            return path
+    raise ValueError(
+        "Unable to resolve the stockfish binary path. Make sure the `STOCKFISH_BINARY` env variable is set."
+    )
 
+
+def config_factory(
+    parsed_args: argparse.Namespace, environ: typing.Mapping
+) -> AppConfig:
+
+    final_engine_path = (
+        parsed_args and parsed_args.engine_path or get_engine_path(environ)
+    )
+    (
+        parsed_args
+        and getattr(parsed_args, "depth", None)
+        or constants.DEFAULT_ENGINE_DEPTH
+    )
     return AppConfig(
         data=DataConfig(),
-        depth=parsed_args
-        and getattr(parsed_args, "depth", None)
-        or constants.DEFAULT_ENGINE_DEPTH,
         engine_path=final_engine_path,
         engine=EngineConfig(
             path=final_engine_path,
